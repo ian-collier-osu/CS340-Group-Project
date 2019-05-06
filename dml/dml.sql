d7d1f3ee822794a38e58afcf609779d4cc78708f
mpandrews <46432743+mpandrews@users.noreply.github.com>
2019-05-06 13:16:08 -0700

    Tweaked DML queries a bit more.

--- dml/dml.sql
+++ dml/dml.sql
@@ -3,55 +3,55 @@
 --INSERTIONS
 
 --Insert a new model
-INSERT INTO models (name, base_trimline) VALUES (:name_in, :trimline_in)
+INSERT INTO models (name, base_trimline) VALUES (:name_in, :trimline_in);
 
 --Insert a new trimline
 
-INSERT INTO trimlines (name, model, default_color) VALUES (:name_in, :model_in, color_in)
+INSERT INTO trimlines (name, model, default_color) VALUES (:name_in, :model_in, color_in);
 
 --Insert a new color
 
-INSERT INTO colors (name) VALUES (:name_in)
+INSERT INTO colors (name) VALUES (:name_in);
 
 --Insert a new part
 
-INSERT INTO parts (name, quantity_on_hand, cost) VALUES (:name_in, :quant_in, :cost_in)
+INSERT INTO parts (name, quantity_on_hand, cost) VALUES (:name_in, :quant_in, :cost_in);
 
 --Insert a new part requirement
 
 --NOTE: Either associated_model or associated_trimline must be NULL.
 
-INSERT INTO part_requirements (associated_model, associated_trimline, associated_part, quantity) VALUES (:model_in, :trim_in; :part_in, :quant_in)
+INSERT INTO part_requirements (associated_model, associated_trimline, associated_part, quantity) VALUES (:model_in, :trim_in; :part_in, :quant_in);
 
 --Insert a new model/color pairing
 
-INSERT INTO model_colors (color, model) VALUES (:color_in, :model_in)
+INSERT INTO model_colors (color, model) VALUES (:color_in, :model_in);
 
 --Add default trimline to model (necessary because a model must exist before a trimline can be created for it)
 
-UPDATE models SET base_trimline = :trim_in WHERE id = :id_in
+UPDATE models SET base_trimline = :trim_in WHERE id = :id_in;
 
 --Insert a new order
 
-INSERT INTO orders (customer, trimline, color) VALUES (:customer_in, :trimline_in, :color_in)
+INSERT INTO orders (customer, trimline, color) VALUES (:customer_in, :trimline_in, :color_in);
 
 --DELETIONS
 
 --Delete an order
 
-DELETE FROM orders WHERE id = :id_in
+DELETE FROM orders WHERE id = :id_in;
 
 --Delete a model/color pairing
 
-DELETE FROM model_colors WHERE color = :color_in AND model = :model_in
+DELETE FROM model_colors WHERE color = :color_in AND model = :model_in;
 
 --Delete a color
 
-DELETE FROM colors WHERE name = :color_in
+DELETE FROM colors WHERE name = :color_in;
 
 --Remove color choice from order (functional deletion of relationship)
 
-UPDATE orders SET color = NULL WHERE id = :id_in
+UPDATE orders SET color = NULL WHERE id = :id_in;
 
 --UPDATES
 
@@ -61,9 +61,29 @@ UPDATE parts SET quantity_on_hand = :quant_in WHERE id = :id_in;
 
 --SELECTS
 
+--Get list of trimlines
+SELECT name FROM trimlines;
+
+--Get list of models
+SELECT name FROM models;
+
+--Get list of colors.
+SELECT name FROM colors;
+
+--Get list of parts.
+
+SELECT name FROM parts;
+
 --Show trimlines available for a given model.
 
-SELECT m.name, t.name FROM models m INNER JOIN trimlines t
+SELECT t.name FROM trimlines t WHERE t.name = :model_in;
+
+--Show colors available for a given model.
+
+SELECT c.name FROM models m
+  INNER JOIN model_colors mc ON m.id = mc.models
+  INNER JOIN colors c ON mc.color = c.name
+  WHERE m.id = :model_in;  
 
 --Show parts and costs for each for a specific trimline.
 
@@ -82,3 +102,22 @@ SELECT o.id, o.customer, m.name, t.name, o.color, SUM(p.cost * pr.quantity) AS p
   INNER JOIN part_requirements pr ON pr.associated_trimline = t.id OR pr.associated_model = m.id
   INNER JOIN parts p ON p.id = pr.associated_part
   WHERE o.id = :order_in;
+
+  --Show parts need to fulfill a specific order, and their costs.
+
+  SELECT p.name, (pr.quantity - p.quantity_on_hand) AS shortfall, ((pr.quantity - p.quantity_on_hand) * p.cost) AS cost_to_order FROM orders o
+    INNER JOIN trimlines t ON o.trimline = t.id
+    INNER JOIN models m ON t.model = m.id
+    INNER JOIN part_requirements pr ON pr.associated_trimline = t.id OR pr.associated_model = m.id
+    INNER JOIN parts p ON p.id = pr.associated_part
+    WHERE o.id = :order_in
+    HAVING shortfall > 0;
+
+  --Show parts needed to fulfill *all* orders, and their costs.
+
+  SELECT p.name, (pr.quantity - p.quantity_on_hand) AS shortfall, ((pr.quantity - p.quantity_on_hand) * p.cost) AS cost_to_order FROM orders o
+    INNER JOIN trimlines t ON o.trimline = t.id
+    INNER JOIN models m ON t.model = m.id
+    INNER JOIN part_requirements pr ON pr.associated_trimline = t.id OR pr.associated_model = m.id
+    INNER JOIN parts p ON p.id = pr.associated_part
+    HAVING shortfall > 0;
