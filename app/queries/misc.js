9a70b5a3d3a2a0e89615623224bab326dc3fc325
ian-collier <iancollier4573@gmail.com>
2019-06-12 19:33:37 -0700

    Re-add search page

--- app/queries/misc.js
+++ app/queries/misc.js
@@ -1,11 +1,12 @@
 exports.searchCustomer = function(con, cb, params) {
     params[0] = '%' + params[0] + '%';
-    con.query(`SELECT o.id, o.customer, m.name, t.name, o.color, SUM(p.cost * pr.quantity) AS part_cost
+    con.query(`SELECT o.id, o.customer, m.name, t.name, c.name, SUM(p.cost * pr.quantity) AS part_cost
       FROM orders o
       INNER JOIN trimlines t ON o.trimline = t.id
       INNER JOIN models m ON t.model = m.id
       INNER JOIN part_requirements pr ON pr.associated_trimline = t.id OR pr.associated_model = m.id
       INNER JOIN parts p ON p.id = pr.associated_part
+      INNER JOIN colors c ON c.id = o.color
       WHERE o.customer LIKE ?`, params, function(err, rows){
         cb(rows, err);
       });
