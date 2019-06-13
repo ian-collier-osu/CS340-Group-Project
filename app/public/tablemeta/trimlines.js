eb943b338104186c9dee138b6b067e692a579035
ian-collier <iancollier4573@gmail.com>
2019-06-12 19:29:18 -0700

    Merge fix'

--- app/public/tablemeta/trimlines.js
+++ app/public/tablemeta/trimlines.js
@@ -3,10 +3,6 @@ var routesData = {
     primaryKey: "id",
     columnMetas: [
         new ColumnMeta("Name", "name", FieldTypeEnum.TEXT),
-<<<<<<< HEAD
-        new ColumnMeta("Model", "model", FieldTypeEnum.FOREIGN_KEY, "/Models", "id", "name")
-=======
         new ColumnMeta("Model", "model", FieldTypeEnum.FOREIGN_KEY, "/Models", "id", "name"),
->>>>>>> master
     ]
 };
