d8a3161e61637b9d88bb1ab25659d06fa8553a7f
ian-collier <iancollier4573@gmail.com>
2019-06-12 19:27:18 -0700

    Remove search js

--- app/public/search.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function() {
-    $RESULT_TABLE = $('#searchTable');
-    $RESULT_TABLE.empty();
-
-    $(document).on('click', '#searchButton', function() {
-        console.log($('#searchString').val());
-    });
-});
